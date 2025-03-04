282583184fd4116ad8ae69ea04c0859944117043
Willmer Jose Jimenez Rodriguez <wiljimen@c1r10s4.42madrid.com>
2025-03-04 18:14:01 +0100

    Trying to fix sort_aux

--- src/sort_functions.c
+++ src/sort_functions.c
@@ -6,7 +6,7 @@
 /*   By: wiljimen <wiljimen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 18:17:23 by wiljimen          #+#    #+#             */
-/*   Updated: 2025/02/18 19:41:26 by wiljimen         ###   ########.fr       */
+/*   Updated: 2025/03/04 17:47:27 by wiljimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,13 +41,13 @@ void sort_aux(t_list **stack_a, t_list **stack_b, int index, int num)
 		temp = temp->next;
 		i++;
 	}
-	if (i == 1)
-		swap(stack_a, 'a');
-	else if (i == 2)
+	if (i == 2)
 		rotate(stack_a, 'a');
-	else if (i == 3)
+	if (i == 1 || i == 2)
+		swap(stack_a, 'a');
+	if (i == 3)
 		rev_rotate(stack_a, 'a');
-	else if (i == 4)
+	if (i == 4 || i == 3)
 	{
 		rev_rotate(stack_a, 'a');
 		if (index == 0 && num != 4)
@@ -95,8 +95,8 @@ void	sort_four_num(t_list **stack_a, t_list **stack_b)
 
 void	sort_five_num(t_list **stack_a, t_list **stack_b)
 {
-	sort_aux(stack_a, stack_b, 0, 5);
-	sort_aux(stack_a, stack_b, 1, 5);
+	sort_aux(stack_a, stack_b, 0, 4);
+	sort_aux(stack_a, stack_b, 1, 4);
 	sort_three_num(stack_a);
 	push(stack_b, stack_a, 'a');
 	push(stack_b, stack_a, 'a');
